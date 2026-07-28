5530e6c5230fba9dbcc027764257b7e5de77d95f
Shrinath <shrinathhariharan@gmail.com>
2026-07-27 20:51:19 -0500

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -44,19 +44,19 @@ int main(int argc, char* argv[]) {
         })},
         {"tools", json::array({
             {
-            "type": "function",
-            "function": {
-                "name": "Read",
-                "description": "Read and return the contents of a file",
-                "parameters": {
-                "type": "object",
-                "properties": {
-                    "file_path": {
-                    "type": "string",
-                    "description": "The path to the file to read"
+            "type", "function",
+            "function", {
+                "name", "Read",
+                "description", "Read and return the contents of a file",
+                "parameters", {
+                "type", "object",
+                "properties", {
+                    "file_path", {
+                    "type", "string",
+                    "description", "The path to the file to read"
                     }
                 },
-                "required": ["file_path"]
+                "required", json::array({"file_path"})
                     }
                 }
             }

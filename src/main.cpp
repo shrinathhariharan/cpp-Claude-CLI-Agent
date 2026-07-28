3c472a24255973a4407ee5b2546d7f1c3fa4be21
Shrinath <shrinathhariharan@gmail.com>
2026-07-27 20:53:45 -0500

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -44,21 +44,21 @@ int main(int argc, char* argv[]) {
         })},
         {"tools", json::array({
             {
-            "type", "function",
-            "function", {
-                "name", "Read",
-                "description", "Read and return the contents of a file",
-                "parameters", {
-                "type", "object",
-                "properties", {
-                    "file_path", {
-                    "type", "string",
-                    "description", "The path to the file to read"
-                    }
-                },
-                "required", json::array({"file_path"})
-                    }
-                }
+                {"type", "function"},
+                {"function", {
+                    {"name", "Read"},
+                    {"description", "Read and return the contents of a file"},
+                    {"parameters", {
+                        {"type", "object"},
+                        {"properties", {
+                            {"file_path", {
+                                {"type", "string"},
+                                {"description", "The path to the file to read"}
+                            }}
+                        }},
+                        {"required", json::array({"file_path"})}
+                    }}
+                }}
             }
         })}
     };

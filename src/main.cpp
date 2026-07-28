dd0651c3a4080d5730f46908f30daf6ecbb33028
Shrinath <shrinathhariharan@gmail.com>
2026-07-28 12:53:14 -0500

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -104,7 +104,7 @@ int main(int argc, char* argv[]) {
     {
         json tool_call = message["tool_calls"][0];
 
-        std:string tool_name = tool_call["function"]["name"].get<std::string>();
+        std::string tool_name = tool_call["function"]["name"].get<std::string>();
 
         json arguments = json::parse(
             tool_call["function"]["arguments"].get<std::string>();
